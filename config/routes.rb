41b383d0e63ce0f2ee43ffa4f457cac877d373c3
Vladislav Shved <vladislav.shved@onthespotdev.comvladislav.shved@onthespotdev.comvladislav.shved@onthespotdev.comvladislav.shved@onthespotdev.comvladislav.shved@onthespotdev.comvladislav.shved@onthespotdev.comvladislav.shved@onthespotdev.com>
2018-11-13 18:18:53 +0300

    add get to route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/backoffice', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/' => 'application#index'
 end
